9e697971f26f66465b63d14363ed9e54846fb8c3
oliverz97 <oliver@mordoor.de>
2019-09-08 11:18:43 +0200

    update data with new request

--- graph_api.js
+++ graph_api.js
@@ -11,10 +11,6 @@ class GraphDBClient {
     }
 
     query(url) {
-        /*let promise = new Promise(function(resolve, reject) {
-            setTimeout(() => resolve(term), 2000);
-        })
-        return promise;*/
         let options = {
             url: url,
             headers: {
@@ -29,7 +25,6 @@ class GraphDBClient {
                 if (err) {
                     reject(err);
                 } else {
-                    //console.log(url);
                     setTimeout(() => resolve(body), 2000);
                 }
             })
@@ -38,9 +33,6 @@ class GraphDBClient {
     }
 
     createURLFromString(term) {
-        if(term = ""){
-            term = "A";
-        }
         let string = graphURL + "?query=PREFIX%20ns%3A%20%3Chttp%3A%2F%2Fexample.com%2Fnamespace%3E%0APREFIX%20foaf%3A%20%3Chttp%3A%2F%2Fxmlns.com%2Ffoaf%2F0.1%2F%3E%0ASELECT%20%3Ffather%20%3Fname%0AWHERE%20%7B%0A%20%20%20%20%3Fx%20a%20%3Chttp%3A%2F%2Fontology.ontotext.com%2Ftaxonomy%2FPerson%3E%20.%0A%20%20%20%20%3Fx%20%3Chttp%3A%2F%2Fontology.ontotext.com%2Ftaxonomy%2Ffather%3E%20%3Ffather%20.%0A%20%20%20%20%3Ffather%20%3Chttp%3A%2F%2Fontology.ontotext.com%2Ftaxonomy%2FhasValue%3E%20%3Fname.%0A%20%20%20%20FILTER%20regex%28%3Fname%2C%20%22" + term + "%22%29%0A%7D"
         console.log(string)
         return string;
@@ -52,21 +44,14 @@ const graphdb = new GraphDBClient();
 
 //app.use(cors);
 
-// app.get('/', (req, res) => {
-//     return res("./index.html")
-// })
-
 app.get('/api/search', (req, res) => {
     let term = req.query.q || '';
-    //console.log("Term: ", term)
     let graphrequest = graphdb.createURLFromString(term)
     let promise = graphdb.query(graphrequest);
     promise.then((result) => {
-        //console.log(result)
         return res.send(result)
     })
 });
-
 
 app.listen(3000, () =>
     console.log("Server listening on Port 3000")
